9c9b16c45fb998f3fb14b63d3c8a1f62f178d6de
HariharanM69 <73754075+HariharanM69@users.noreply.github.com>
2024-07-02 14:11:27 +0000

    test.py

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import streamlit as st
+from PIL import Image
 from openai import OpenAI
 
 st.title("Vision Assistant")
-picture = st.camera_input("",help="Show anything within this frame")
+# picture = st.camera_input("",help="Show anything within this frame")
+
+picture = st.image('lol.png')
 
 if picture:
     st.write("Execute")
-    st.image(picture)
+    st.image(Image.open(picture))
