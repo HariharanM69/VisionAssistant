e9eb26000dad60cbaed689c9dd902137923ff749
HariharanM69 <73754075+HariharanM69@users.noreply.github.com>
2024-07-02 14:17:38 +0000

    test.py

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import streamlit as st
-from PIL import Image
+import base64
+import requests
 from openai import OpenAI
 
 st.title("Vision Assistant")
 # picture = st.camera_input("",help="Show anything within this frame")
-
+def encode_image(file_path):
+  with open(file_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+      
 picture = st.image('lol.png')
 
 if picture:
     st.write("Execute")
-    st.image(Image.open(picture))
+    st.image(encode_image(picture))
