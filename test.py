b4b7b3cc7077389f0054427962586e2d5e192ff6
HariharanM69 <73754075+HariharanM69@users.noreply.github.com>
2024-07-02 09:56:13 +0000

    test.py

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
 import streamlit as st
-from streamlit.runtime.uploaded_file_manager import UploadedFile
+from openai import OpenAI
 
+st.title("Vision Assistant")
+picture = st.camera_input("",help="Show anything within this frame")
 
-def main():
-    img_file_buffer = st.camera_input("Take a picture")
-
-    img_file = UploadedFile(img_file_buffer.getvalue(), "image/jpeg",
-                            img_file_buffer.name)
-    st.image(img_file)
-
-
-if __name__ == "__main__":
-    main()
+if picture == True:
+    st.write("Execute")
