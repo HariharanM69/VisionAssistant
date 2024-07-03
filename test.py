42dac001ca573050996186196f345d02736ec289
HariharanM69 <73754075+HariharanM69@users.noreply.github.com>
2024-07-03 10:34:14 +0000

    Implemented OpenAI's TTS

--- test.py
+++ test.py
@@ -1,41 +1,54 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import base64
 import io
-# from PIL import Image as img
 from openai import OpenAI
 
 honey = OpenAI(api_key=st.secrets["OPENAI"]["OPENAI_API_KEY"])
 
 
+# def speech2text(voice):
+
+
+def tts(description):
+    response = honey.audio.speech.create(
+        model="tts-1",
+        voice="nova",
+        input=description
+    )
+    voice = response.content
+    return io.BytesIO(voice)
+
+
 def encode_image(image_data):
-   return base64.b64encode(image_data).decode('utf-8')
+    return base64.b64encode(image_data).decode('utf-8')
 
 
-def vision_file(file_path):
-   base64_image = encode_image(file_path)
-   response = honey.chat.completions.create(
-       model="gpt-4o",
-       messages=[{
-           "role":
-           "system",
-           "content":
-           """
-        You are an image analysis assistant powered by GPT-4 with vision capabilities. 
-        Your task is to identify and describe objects in images provided by the user. 
-        """
-       }, {
-           "role":
-           "user",
-           "content": [{
-               "type": "image_url",
-               "image_url": {
-                   "url": f"data:image/jpeg;base64,{base64_image}"
-               }
-           }]
-       }],
-       max_tokens=300,
-   )
-   return (response.choices[0].message.content)
+def vision(file_path):
+    base64_image = encode_image(file_path)
+    response = honey.chat.completions.create(
+        model="gpt-4o",
+        messages=[{
+            "role":
+                "system",
+            "content":
+                """
+             You are an image analysis assistant powered by GPT-4 with vision capabilities.
+             Your task is to identify and describe objects in images provided by the user.
+             """
+        }, {
+            "role":
+                "user",
+            "content": [{
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/jpeg;base64,{base64_image}"
+                }
+            }]
+        }],
+        max_tokens=300,
+    )
+    return response.choices[0].message.content
 
 
 st.title("Vision Assistant")
@@ -44,12 +57,26 @@ image = st.camera_input('', help="Show anything within this frame")
 # image = "lol.png"
 
 if image:
-   st.write("Executing")
-   image_data = image.getvalue()
-   st.image(image_data, caption='Captured Image', use_column_width=True)
-   try:
-        description = vision_file(image_data)
+    st.write("Executing")
+    image_data = image.getvalue()
+    # st.image(image_data, caption='Captured Image', use_column_width=True)
+    try:
+        description = vision(image_data)
         st.write("GPT-4 Analysis Result:")
+        voice = tts(description)
+        audio_base64 = base64.b64encode(voice.read()).decode('utf-8')
+        components.html(f"""
+                <section hidden>
+                    <audio id="audio" controls>
+                        <source src="data:audio/mp3;base64,{audio_base64}" type="audio/mp3">
+                    </audio>
+                </section>
+                <script>
+            var audioElement = document.getElementById('audio');
+            audioElement.currentTime = 0; // Ensure playback starts from the beginning
+            audioElement.play();
+        </script>
+                """,height=0)
         st.write(description)
-   except Exception as e:
+    except Exception as e:
         st.error(f"An error occurred: {e}")
